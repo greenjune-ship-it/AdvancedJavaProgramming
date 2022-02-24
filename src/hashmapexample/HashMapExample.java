04b7ba61b148835d1fdbd1505612797307597d4a
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-02-24 19:09:57 +0100

    03_04

---
+++ src/hashmapexample/HashMapExample.java
@@ -0,0 +1,25 @@
+package hashmapexample;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> phonebook = new HashMap<>();
+        phonebook.put("Kevin", 12345);
+        phonebook.put("Jill", 98765);
+        phonebook.put("Brenda", 123123);
+        // Brenda will be overwritten without error
+        phonebook.put("Brenda", 22222);
+        // null might be a key
+        phonebook.put(null, 000);
+        if (phonebook.containsKey("Brenda")) {
+            phonebook.remove("Brenda");
+        }
+        phonebook.clear();
+        System.out.println(phonebook);
+
+    }
+
+}
