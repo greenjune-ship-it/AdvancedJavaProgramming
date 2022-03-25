3028976b99d3132596bc6abb9d4456efe86cc58d
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-03-25 12:16:27 +0100

    07_03

---
+++ src/multithreading_in_java/bufferedreaderexample/BufferedReaderExample.java
@@ -0,0 +1,25 @@
+package multithreading_in_java.bufferedreaderexample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderExample {
+
+    public static void main(String[] args) {
+
+        File myFile = new File("data/example.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(myFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+
+        }
+
+    }
+
+}
